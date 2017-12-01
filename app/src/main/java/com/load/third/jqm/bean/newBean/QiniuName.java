34581f26d913db102038699d6c9e779b26bc443c
liulei <649444395@qq.com>
2017-12-01 11:09:35 +0800

    完成首页的新老接口替换

---
+++ app/src/main/java/com/load/third/jqm/bean/newBean/QiniuName.java
@@ -0,0 +1,20 @@
+package com.load.third.jqm.bean.newBean;
+
+/**
+ * 用途：
+ * 作者：Created by liulei on 2017/12/1.
+ * 邮箱：liulei2@aixuedai.com
+ */
+
+
+public class QiniuName {
+    private String fileName;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
---
+++ app/src/main/java/com/load/third/jqm/bean/newBean/QiniuToken.java
@@ -0,0 +1,20 @@
+package com.load.third.jqm.bean.newBean;
+
+/**
+ * 用途：
+ * 作者：Created by liulei on 2017/12/1.
+ * 邮箱：liulei2@aixuedai.com
+ */
+
+
+public class QiniuToken {
+    private String uptoken;
+
+    public String getUptoken() {
+        return uptoken;
+    }
+
+    public void setUptoken(String uptoken) {
+        this.uptoken = uptoken;
+    }
+}
