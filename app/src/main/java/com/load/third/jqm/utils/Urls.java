dc1f2583cbe5524b5845540d6e825f5311b253b8
liulei <649444395@qq.com>
2017-12-03 14:25:48 +0800

    替换请求接口

--- app/src/main/java/com/load/third/jqm/utils/Urls.java
+++ app/src/main/java/com/load/third/jqm/utils/Urls.java
@@ -5,15 +5,17 @@ package com.load.third.jqm.utils;
  */
 
 public class Urls {
-    public static final String url_checkPhone="logged/GetMessage";
-    public static final String url_getQiNiuToken="QiniuToken";
-    public static final String url_getQiNiuName="QiniuReName";
-    public static final String BASE_URL="http://jqm.daodaok.com/";
-    public static final String url_repayment=BASE_URL+"loan/logged/PayAction";
-    public static final String url_help=BASE_URL+"app/loanHelper.html";
-    public static final String url_aboutUs=BASE_URL+"aboutRapidTransfer.html";
-    public static final String url_agreement=BASE_URL+"app/loanAgreement.html";
-    public static final String url_xuexin_verify=BASE_URL+"loanEducation/logged/StudyAuth";
-    public static final String url_open_qq="mqqwpa://im/chat?chat_type=wpa&uin=";
-    public static final String url_tianji_check_success="checking.html";
+    public static final String url_checkPhone = "logged/GetMessage";
+    public static final String url_getQiNiuToken = "QiniuToken";
+    public static final String url_getQiNiuName = "QiniuReName";
+    //    public static final String BASE_URL="http://jqm.daodaok.com/";
+    public static final String BASE_URL = "http://jqm.****.com/";
+
+    public static final String url_repayment = BASE_URL + "loan/logged/PayAction";
+    public static final String url_help = BASE_URL + "app/loanHelper.html";
+    public static final String url_aboutUs = BASE_URL + "aboutRapidTransfer.html";
+    public static final String url_agreement = BASE_URL + "app/loanAgreement.html";
+    public static final String url_xuexin_verify = BASE_URL + "loanEducation/logged/StudyAuth";
+    public static final String url_open_qq = "mqqwpa://im/chat?chat_type=wpa&uin=";
+    public static final String url_tianji_check_success = "checking.html";
 }
