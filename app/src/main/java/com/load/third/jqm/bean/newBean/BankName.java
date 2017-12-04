b6eab4d9bd09cfa8ebe6dfd883deff3e8376167c
liulei <649444395@qq.com>
2017-12-04 20:02:41 +0800

    类丢失

---
+++ app/src/main/java/com/load/third/jqm/bean/newBean/BankName.java
@@ -0,0 +1,20 @@
+package com.load.third.jqm.bean.newBean;
+
+/**
+ * 用途：
+ * 作者：Created by liulei on 2017/12/4.
+ * 邮箱：liulei2@aixuedai.com
+ */
+
+
+public class BankName {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
